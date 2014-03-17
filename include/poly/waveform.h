0a7ff92a0db1feac7c15623b3c958c9c438fe20d
Travis Whitaker <pi.boy.travis@gmail.com>
2014-03-17 16:34:52 -0400

    Whoops, didn't mean to keep that.

--- include/poly/waveform.h
+++ include/poly/waveform.h
@@ -4,7 +4,7 @@
 #ifndef POLY_WAVEFORM_H
 #define POLY_WAVEFORM_H
 
-#define POLY_MAX_AMP 32766.0
+#define POLY_MAX_AMP 32768.0
 
 float poly_sine(float amplitude, float freq, float phase);
 float poly_square(float amplitude, float freq, float duty, float phase);
